9185b81efa447cd36940f6f66e4a547da4758146
david padrino <david.padrino3@hotmail.com>
2017-01-08 19:12:30 -0400

    first commit

---
+++ collections/Recipes.js
@@ -0,0 +1,66 @@
+Recipes = new Mongo.Collection('recipes');
+
+// Grant insert permissions only if an user exists and is logged in
+Recipes.allow({
+	insert: function(userId, doc) {
+		return !!userId;
+	}
+});
+
+Ingredient = new SimpleSchema({
+	name: {
+		type: String,
+	},
+	amount: {
+		type: String,
+	}
+});
+
+
+
+RecipeSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name"
+	},
+
+	desc: {
+		type: String,
+		label: "Description"
+	},
+	ingredients: {
+		type: [Ingredient]
+	},
+	inMenu: {
+		type: Boolean,
+		defaultValue: false,
+		optional: true,
+		autoform: {
+			type: "hidden"
+		}
+	},
+	author:{
+		type: String,
+		label: "Author",
+		autoValue: function() {
+			return this.userId
+		},
+		autoform: {
+			type: "hidden",
+		} 
+	},
+
+	createdAt: {
+		type: Date,
+		label: "Created at",
+		autoValue: function() {
+			return new Date()
+		},
+		autoform: {
+			type: "hidden",
+		} 
+	}
+});
+
+
+Recipes.attachSchema( RecipeSchema );
