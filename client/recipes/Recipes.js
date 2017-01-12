71d12486fb019bebc191e44fbd5f50c57b96a14b
david padrino <david.padrino3@hotmail.com>
2017-01-11 22:06:14 -0400

    adelanto

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,1 +1,16 @@
-Meteor.subscribe('recipes');
+// Meteor.subscribe('recipes');
+
+//Template level subscription replaces the Meteor.subscribe command
+Template.Recipes.onCreated(function() {
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes');
+	});
+
+});
+
+Template.Recipes.helpers({
+	recipes:()=>{
+		return Recipes.find({});
+	}
+});
